0fd1cde2099a8499f2b9853964ac906fcfe3f556
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-17 19:48:13 -0300

    feat: load fonts into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export function App() {
+  const [areFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!areFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
@@ -17,5 +34,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'GeneralSans-400',
   },
 });
