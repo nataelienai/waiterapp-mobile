b29f396c64352a299ce1486ef1dbcef0328d6e87
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-18 15:03:30 -0300

    feat: create Main component structure

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,24 @@
 import { Header } from '../components/Header';
 
-import { Container } from './styles';
+import {
+  CategoriesContainer,
+  Container,
+  Footer,
+  FooterContainer,
+  MenuContainer,
+} from './styles';
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer />
+        <MenuContainer />
+      </Container>
+      <Footer>
+        <FooterContainer />
+      </Footer>
+    </>
   );
 }
